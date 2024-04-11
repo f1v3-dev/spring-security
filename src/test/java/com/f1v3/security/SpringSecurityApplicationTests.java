1caf0564371424085f7fee78e13de080a6cd33ab
f1v3-dev <f1v3_dev@naver.com>
2024-04-11 15:32:06 +0900

    add: init

---
+++ src/test/java/com/f1v3/security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1v3.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
