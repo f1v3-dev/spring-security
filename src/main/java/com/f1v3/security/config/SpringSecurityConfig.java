b978ed3981ca2c68274b412944e76f6dc5495089
f1v3-dev <f1v3_dev@naver.com>
2024-04-15 15:56:35 +0900

    feat: Custom Login Page 설정

--- src/main/java/com/f1v3/security/config/SpringSecurityConfig.java
+++ src/main/java/com/f1v3/security/config/SpringSecurityConfig.java
@@ -18,19 +18,19 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화 ->
+                .cors(AbstractHttpConfigurer::disable) // cors 비활성화
                 .authorizeHttpRequests(request -> request
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .anyRequest().authenticated() // 모든 요청에 대해 인증을 오규
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll() // forward 요청은 모든 사용자에게 허용 ?
+                        .anyRequest().authenticated() // 모든 요청에 대해 인증을 요구
                 )
                 .formLogin(login -> login  // form 로그인 설정
-                        .loginPage("/view/login")
-                        .loginProcessingUrl("/login-process")
-                        .usernameParameter("userId")
-                        .passwordParameter("password")
-                        .defaultSuccessUrl("/view/dashboard", true)
-                        .permitAll()
+                        .loginPage("/view/login") // thymeleaf view 경로
+                        .loginProcessingUrl("/login-process") // @PostMapping("/login-process") -> 로그인 처리 Controller
+                        .usernameParameter("userId") // Thymeleaf 내의 userId name -> 회원의 아이디
+                        .passwordParameter("password") // Thymeleaf 내의 password name -> 회원의 비밀번호
+                        .defaultSuccessUrl("/view/dashboard", true) // 로그인 성공 시 이동할 URL
+                        .permitAll() // 로그인 페이지는 모든 사용자에게 허용!
                 )
                 .logout(Customizer.withDefaults()); // 로그아웃은 기본 설정 (/logout -> 인증 해제)
 
